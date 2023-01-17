4ba09927a57521844b6e62621f9bd940e17ed2dc
Aleksei Shatalov <aalleexsh2011@yandex.ru>
2023-01-17 16:13:37 +0200

    DB confug changed

--- server/src/db.ts
+++ server/src/db.ts
@@ -1,11 +1,11 @@
 import { Pool } from 'pg';
-import { DB_PORT, DB_NAME } from './utils/config';
+import { DB_PORT, DB_NAME, DB_PASS } from './utils/config';
 
 const pool = new Pool({
-	user: 'postgres',
-	host: 'localhost',
+	user: 'matcha_user',
+	host: 'dpg-cf39hthgp3jl0q3k9cgg-a.frankfurt-postgres.render.com',
 	database: DB_NAME,
-	password: 'postgres',
+	password: DB_PASS,
 	port: DB_PORT
 });
 
--- server/src/utils/config.ts
+++ server/src/utils/config.ts
@@ -1,5 +1,10 @@
-const DB_PORT: number = process.env.NODE_ENV === 'test' ? 5433 : 5432;
+// const DB_PORT: number = process.env.NODE_ENV === 'test' ? 5433 : 5432;
 
-const DB_NAME: string = process.env.NODE_ENV === 'test' ? 'matcha-test' : 'matcha';
+// const DB_NAME: string = process.env.NODE_ENV === 'test' ? 'matcha-test' : 'matcha';
+const DB_PORT = 5432;
 
-export { DB_NAME, DB_PORT };
+const DB_NAME = 'matcha';
+
+const DB_PASS = process.env.DB_PASS;
+
+export { DB_NAME, DB_PORT, DB_PASS };
