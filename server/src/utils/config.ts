cb1fe1fcafe82e22159368284419451c902e2855
Aleksei Shatalov <aalleexsh2011@yandex.ru>
2023-01-17 18:31:35 +0200

    refactor

--- server/src/utils/config.ts
+++ server/src/utils/config.ts
@@ -1,10 +1,10 @@
 // const DB_PORT: number = process.env.NODE_ENV === 'test' ? 5433 : 5432;
 
 // const DB_NAME: string = process.env.NODE_ENV === 'test' ? 'matcha-test' : 'matcha';
-const DB_PORT = 5432;
 
-const DB_NAME = 'matcha';
 
-const DB_PASS = process.env.DB_PASS;
+const DB_PORT: number = process.env.NODE_ENV === 'test' ? Number(process.env.DB_PORT_TEST) : Number(process.env.DB_PORT);
 
-export { DB_NAME, DB_PORT, DB_PASS };
+const DB_NAME: string = process.env.NODE_ENV === 'test' ? String(process.env.DB_NAME_TEST) : String(process.env.DB_NAME);
+
+export { DB_NAME, DB_PORT };
