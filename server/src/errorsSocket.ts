57fd38fd7881b4af836fe5de03610f8ba4a733b1
Aleksei Shatalov <aalleexsh2011@yandex.ru>
2023-01-17 20:47:16 +0200

    sielence neveronline

--- server/src/errorsSocket.ts
+++ server/src/errorsSocket.ts
@@ -6,7 +6,7 @@ export const socketErrorHandler = (handler: any) => {
 	const handleError = (err: any) => {
 		if (err.message === 'No record of this user being active'){
 			void err;
-			console.log('never online');
+			// console.log('never online');
 		} else {
 			console.error('please handle me', err);
 		}
